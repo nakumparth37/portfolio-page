b0a60cfd60b379653cc5fea7ff84c231946fd248
Parth Nakum <75476666+nakumparth37@users.noreply.github.com>
2023-02-27 22:23:31 +0530

    make  the portfoloi

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?auto=compress&cs=tinysrgb&w=1600')",
+      },
+      colors: {
+        'btn': '#cd081e',
+      },
+  }
+  },
+  plugins: [],
+}
+
